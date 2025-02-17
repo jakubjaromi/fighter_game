00cfc4e5df9d28c2b0d70268b856a4874ee399a7
jakubjaromi <jakub.jaromi@gmail.com>
2025-02-17 14:01:22 +0100

    fourth draft

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Fighter')
 clock = pygame.time.Clock()
 boss_font = pygame.font.Font('font/Minecraft.ttf', 36)
+menu_font = pygame.font.Font('font/Minecraft.ttf', 50)
 scale = 0.8
 moving_left = False
 moving_right = False
 moving_attack = False
 life_status = 5
-
+game_init = True
+easy_arrow = ' <'
+medium_arrow = ''
+hard_arrow = ''
+max_fireballs = 3
 
 class Background(pygame.sprite.Sprite):
 
@@ -31,7 +36,7 @@ class Background(pygame.sprite.Sprite):
 
 
 class Heart(pygame.sprite.Sprite):
-    def __init__(self, scale):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
         img_1 = pygame.image.load('graphics/heart_1.png').convert_alpha()
@@ -43,7 +48,7 @@ class Heart(pygame.sprite.Sprite):
                                        (int(img_2.get_width() * scale), int(img_2.get_height() * scale)))
         self.image_list = [img_1, img_2]
 
-    def draw(self, life_status):
+    def draw(self):
         current_life_status = life_status
         positions = [(10, 10), (60, 10), (110, 10), (160, 10), (210, 10)]
         for position in positions:
@@ -74,13 +79,13 @@ class Boss(pygame.sprite.Sprite):
 
         #
         img_1 = pygame.image.load('graphics/boss_1.png').convert_alpha()
-        img_1 = pygame.transform.scale(img_1, (int(img_1.get_width() * 1.1), int(img_1.get_height())))
+        img_1 = pygame.transform.scale(img_1, (int(img_1.get_width() * 0.9), int(img_1.get_height() * 0.9)))
 
         img_2 = pygame.image.load('graphics/boss_2.png').convert_alpha()
-        img_2 = pygame.transform.scale(img_2, (int(img_2.get_width() * 1.1), int(img_2.get_height())))
+        img_2 = pygame.transform.scale(img_2, (int(img_2.get_width() * 0.9), int(img_2.get_height() * 0.9)))
 
         img_3 = pygame.image.load('graphics/boss_3.png').convert_alpha()
-        img_3 = pygame.transform.scale(img_3, (int(img_3.get_width() * 1.1), int(img_3.get_height())))
+        img_3 = pygame.transform.scale(img_3, (int(img_3.get_width() * 0.9), int(img_3.get_height() * 0.9)))
         self.image_list = [img_1, img_2, img_3]
         self.image = self.image_list[self.image_index]
         self.rect = self.image.get_rect()
@@ -107,16 +112,21 @@ class Boss(pygame.sprite.Sprite):
         screen.blit(text_hp, (SCREEN_WIDTH - 400, 25))
 
         # draw boss's fireballs
-        max_fireballs = 5
         self.cooldown()
         if self.cool_down_count == 0:
             self.cool_down_count = 1
             if len(self.fireball_list) < max_fireballs:
+                if max_fireballs == 3:
+                    fireballs_speed = random.randint(2, 5)
+                elif max_fireballs == 4:
+                    fireballs_speed = random.randint(3, 7)
+                else:
+                    fireballs_speed = random.randint(4, 10)
                 self.rect_fireball.x = self.rect.x - random.randint(20, 40)
                 self.rect_fireball.y = self.rect.y - random.randint(100, 300)
                 self.fireball_list.append([self.rect.x - random.randint(20, 40),
-                                           self.rect.y + random.randint(0, 500),
-                                           random.randint(2, 5),
+                                           self.rect.y + random.randint(0, 450),
+                                           fireballs_speed,
                                            0])
 
         temp_fireball_list = list()
@@ -149,6 +159,30 @@ class Boss(pygame.sprite.Sprite):
             self.cool_down_count = 0
         elif self.cool_down_count > 0:
             self.cool_down_count += 1
+
+    def check_fireball_hit(self, rect_hero):
+        global life_status
+        temp_fireball_list = list()
+        if self.fireball_list:
+            for fireball in self.fireball_list:
+                current_fireball_x = fireball[0]
+                current_fireball_y = fireball[1]
+                current_fireball_speed = fireball[2]
+                current_fireball_index = fireball[3]
+                image_fireball = self.image_fireball_list[int(current_fireball_index)]
+
+                rect_fireball = image_fireball.get_rect()
+                rect_fireball.x = current_fireball_x
+                rect_fireball.y = current_fireball_y
+
+                if rect_fireball.colliderect(rect_hero):
+                    life_status = life_status - 1
+                    pygame.time.delay(500)
+                else:
+                    temp_fireball_list.append([current_fireball_x, current_fireball_y,
+                                               current_fireball_speed, current_fireball_index])
+
+        self.fireball_list = temp_fireball_list.copy()
 
 
 class Hero(pygame.sprite.Sprite):
@@ -232,8 +266,8 @@ class Hero(pygame.sprite.Sprite):
         self.rect.y += self.gravity
 
         # limit for bottom value
-        if self.rect.bottom >= int(SCREEN_HEIGHT - self.image.get_height()):
-            self.rect.bottom = int(SCREEN_HEIGHT - self.image.get_height())
+        if self.rect.bottom >= int(SCREEN_HEIGHT - self.image.get_height() - 15):
+            self.rect.bottom = int(SCREEN_HEIGHT - self.image.get_height() - 15)
 
         # limit for top value
         if self.rect.top < 0:
@@ -278,7 +312,7 @@ class Hero(pygame.sprite.Sprite):
                 rect_axe.y = current_thrown_axe_y
                 screen.blit(image_axe, rect_axe)
 
-                if rect_axe.x < SCREEN_WIDTH and rect_axe.x > 0:
+                if SCREEN_WIDTH > rect_axe.x > 0:
                     temp_thrown_axe_list.append([current_thrown_axe_x, current_thrown_axe_y,
                                                  current_thrown_axe_direction, current_thrown_axe_index])
 
@@ -290,11 +324,40 @@ class Hero(pygame.sprite.Sprite):
         elif self.cool_down_count > 0:
             self.cool_down_count += 1
 
+
+class Menu(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+    @staticmethod
+    def draw_init():
+        text_difficulty_level = menu_font.render(f'Please select difficulty level: ', False, 'Black').convert()
+        text_easy = menu_font.render(f'Easy{easy_arrow}', False, 'Black').convert()
+        text_medium = menu_font.render(f'Medium{medium_arrow}', False, 'Black').convert()
+        text_hard = menu_font.render(f'Hard{hard_arrow}', False, 'Black').convert()
+
+        screen.blit(text_difficulty_level, (int(SCREEN_WIDTH / 2) - 300, 100))
+        screen.blit(text_easy, (int(SCREEN_WIDTH / 2) - 50, 300))
+        screen.blit(text_medium, (int(SCREEN_WIDTH / 2) - 50, 400))
+        screen.blit(text_hard, (int(SCREEN_WIDTH / 2) - 50, 500))
+
+    @staticmethod
+    def draw_win():
+        text_win = menu_font.render(f'You WIN! Do you want to try again? (press y/Y)', False, 'White').convert()
+        screen.blit(text_win, (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2)))
+
+    @staticmethod
+    def draw_lose():
+        text_lose = menu_font.render(f'You LOST! Do you want to try again? (press y/Y)', False, 'White').convert()
+        screen.blit(text_lose, (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2)))
+
+
 # create background
 bg = Background()
 
 # create heart
-hp = Heart(scale)
+hp = Heart()
 
 # create boss
 boss = Boss()
@@ -309,38 +372,88 @@ while run:
     # draw background
     bg.draw()
 
-    # draw hp hearts
-    hp.draw(life_status)
+    if game_init:
+        Menu.draw_init()
 
-    # draw boss
-    boss.draw()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+                pygame.quit()
 
-    # draw hero
-    hero.draw(moving_attack)
-    hero.move()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    if easy_arrow != '':
+                        easy_arrow = ''
+                        medium_arrow = ' <'
+                        hard_arrow = ''
+                    elif medium_arrow != '':
+                        easy_arrow = ''
+                        medium_arrow = ''
+                        hard_arrow = ' <'
+                    elif hard_arrow != '':
+                        easy_arrow = ' <'
+                        medium_arrow = ''
+                        hard_arrow = ''
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-            pygame.quit()
+                if event.key == pygame.K_UP:
+                    if easy_arrow != '':
+                        easy_arrow = ''
+                        medium_arrow = ''
+                        hard_arrow = ' <'
+                    elif medium_arrow != '':
+                        easy_arrow = ' <'
+                        medium_arrow = ''
+                        hard_arrow = ''
+                    elif hard_arrow != '':
+                        easy_arrow = ''
+                        medium_arrow = ' <'
+                        hard_arrow = ''
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                moving_left = True
-            if event.key == pygame.K_RIGHT:
-                moving_right = True
-            if event.key == pygame.K_LALT:
-                hero.jump()
-            if event.key == pygame.K_LSHIFT:
-                moving_attack = True
+                if event.key == pygame.K_RETURN:
+                    if easy_arrow != '':
+                        max_fireballs = 3
+                    elif medium_arrow != '':
+                        max_fireballs = 4
+                    elif hard_arrow != '':
+                        max_fireballs = 5
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                moving_left = False
-            if event.key == pygame.K_RIGHT:
-                moving_right = False
-            if event.key == pygame.K_LSHIFT:
-                moving_attack = False
+                    game_init = False
+    else:
+
+        boss.check_fireball_hit(hero.rect)
+
+        # draw hp hearts
+        hp.draw()
+
+        # draw boss
+        boss.draw()
+
+        # draw hero
+        hero.draw(moving_attack)
+        hero.move()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+                pygame.quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    moving_left = True
+                if event.key == pygame.K_RIGHT:
+                    moving_right = True
+                if event.key == pygame.K_LALT:
+                    hero.jump()
+                if event.key == pygame.K_LSHIFT:
+                    moving_attack = True
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    moving_left = False
+                if event.key == pygame.K_RIGHT:
+                    moving_right = False
+                if event.key == pygame.K_LSHIFT:
+                    moving_attack = False
 
     # update everything
     pygame.display.update()
